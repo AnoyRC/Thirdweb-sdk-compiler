d96bedca64fd255fb1abdc6a8b99dc65e4befbf3
AnoyRC <38689344+AnoyRC@users.noreply.github.com>
2023-01-29 00:36:29 +0530

    Pack initialization

--- index.ts
+++ index.ts
@@ -11,37 +11,354 @@ const main = async () => {
   try {
     const sdk = ThirdwebSDK.fromPrivateKey(
       process.env.PRIVATE_KEY as string,
-      "goerli"
+      "mumbai"
     );
 
-    const contractAddress = await sdk.deployer.deployNFTDrop({
-      name: "My Drop",
-      primary_sale_recipient: "0x39Ab29fAfb5ad19e96CFB1E1c492083492DB89d4",
-    });
+    const edition = await sdk.getContract("0xb48fAf5A2B3Ef7a28919AB45e93e9Da1F90dA598","edition")
 
-    console.log("Contract address: ", contractAddress);
+    const pack = await sdk.getContract("0x2dDCb0F088312eEa656783FCbdC7e20F8Fd9cbE5","pack")
 
-    const contract = await sdk.getContract(contractAddress, "nft-drop");
+    await edition.setApprovalForAll(pack.getAddress(), true)
 
-    const metadatas = [
-      {
-        name: "Blue Star",
-        description: "A blue star NFT",
-        image: readFileSync("assets/blue-star.png"),
+    await pack.create({
+      packMetadata:{
+        name: "SK_Lootbox",
+        image: readFileSync("./assets/SilverKnightsRender.png"),
+        description: "This lootbox randomly drops card skin from Silver Knights Pack"
       },
-      {
-        name: "Red Star",
-        description: "A red star NFT",
-        image: readFileSync("assets/red-star.png"),
-      },
-      {
-        name: "Yellow Star",
-        description: "A yellow star NFT",
-        image: readFileSync("assets/yellow-star.png"),
-      },
-    ];
+      erc1155Rewards:[
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 0
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 1
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 2
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 3
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 4
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 5
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 6
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 7
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 8
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 9
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 10
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 11
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 12
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 13
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 14
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 15
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 16
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 17
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 18
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 19
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 20
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 21
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 22
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 23
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 24
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 25
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 26
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 27
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 28
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 29
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 30
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 31
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 32
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 33
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 34
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 35
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 36
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 37
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 38
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 39
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 40
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 41
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 42
+        },
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 43
+        },
 
-    await contract.createBatch(metadatas);
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 44
+        },
+
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 45
+        },
+
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 46
+        },
+
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 47
+        },
+
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 48
+        },
+
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 49
+        },
+
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 50
+        },
+
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 51
+        },
+
+        {
+          contractAddress: edition.getAddress(),
+          totalRewards: 200,
+          quantityPerReward: 1,
+          tokenId: 52
+        },
+
+
+      ]
+    })
+
     console.log("Created batch successfully!");
   } catch (e) {
     console.error("Something went wrong: ", e);
